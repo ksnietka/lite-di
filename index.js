284fffcdd4ad73eeeb86f7547a28091bc4d1f32a
Krzysztof Snietka <krzysztof.snietka@L01014968.local>
2019-04-15 08:52:44 +0200

    Refactor of chaining function

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 const Container = require('./lib/Container');
 const resolvers = require('./lib/resolvers');
 
+a = {a: 1}
+
+
+function res (a) {
+    console.log(a)
+    a.a = 2
+
+}
+
+function cons (a) {
+    console.log(a);
+}
+
+
+let container = new Container();
+container.bind('name', []).to(a);
+container.bind('resolves', ['name']).toFunction(res);
+container.bind('cons', ['name']).toFunction(cons);
+
+let resolves1 = container.get('resolves');
+resolves1()
+
+let resolves2 = container.get('resolves');
+resolves2();
+
+let c = container.get('cons');
+c()
+
+
+
+
+
+
+
+
 module.exports = {Container, resolvers};
